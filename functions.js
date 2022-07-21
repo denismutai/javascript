8ba6af72d255ded8387b3d918082beef5a7a4df6
Denis Mutai <denismutai5@gmail.com>
2022-07-21 11:10:11 +0300

    Learn JavaScript functions

---
+++ functions.js
@@ -0,0 +1,6 @@
+// Javascript functions
+const myArray=['I', 'love', 'chocolate', 'frogs'];
+const madeAString=myArray.join(' ');
+console.log(madeAString)
+const myNum=Math.random();
+console.log(myNum);
